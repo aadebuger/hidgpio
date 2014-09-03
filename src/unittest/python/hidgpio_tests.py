bab670b4fd5d664f75f566c535ef1600331d79ed
aadebuger <admin@local.host>
2014-09-03 16:58:07 +0800

    first commit

---
+++ src/unittest/python/hidgpio_tests.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2014年9月3日
+
+@author: aadebuger
+'''
+import unittest
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
